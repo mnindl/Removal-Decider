71871c2aaa6f2a935e47892f18c05de177a66716
mnindl <mnindl75@googlemail.com>
2011-03-31 15:24:23 +0200

    new version

--- removal_decider.js
+++ removal_decider.js
@@ -1,15 +1,45 @@
 google.load("jquery", "1.5.1");
-google.load("maps", "2");
+google.load("jqueryui", "1.8.11");
+google.load("maps", "3", {other_params: "sensor=false"});
 
 google.setOnLoadCallback(function() {
   rooms = null;
   member = null;
-  var dirn = new GDirections();
-  $('#removal_form').submit(function(event) {
+  var directions_service = new google.maps.DirectionsService();
+  var geocoder = new google.maps.Geocoder();
+  // input click
+  $("#removal_form input:text").click( function () {
+    $(this).val("").unbind("click");
+  });
+  $("#removal_form .submit").click(function() {
+    calculateRemoval();
+  });
+  
+  // autocomplete
+  $(".address").autocomplete({
+    source: function(request, response) {
+       // gewichtung der geocodierung auf Deutschland setzen
+      geocoder.geocode( {'address': request.term, 'region': 'de','language': 'de', 'bounds':new google.maps.LatLngBounds(new google.maps.LatLng(54.393352,10.415039 ),new google.maps.LatLng(14.332516,67.631836)) },
+        function(results, status) {
+          response($.map(results, function(item) {
+            return {
+              label: item.formatted_address,
+              value: item.formatted_address
+            }
+         }));
+       })
+     }
+   });
+  //submit
+  var calculateRemoval = function() {
     console.log("form submit");
-    event.preventDefault();
     var start = $("#start").val();
-    var target = $("#target").val();
+    var end = $("#end").val();
+    var route_request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+    };
     member = parseInt($("#member").val());
     rooms = parseInt($("#rooms").val());
     if (isNaN(member) || member.length == 0 || isNaN(rooms) || rooms.length == 0  ) {
@@ -17,69 +47,73 @@ google.setOnLoadCallback(function() {
       return false;
     }
     console.log("-----------> member "+member+" romms "+rooms);
-    dirn.clear();
-    var load_direction = "from: "+start+" to: "+target;
-    dirn.load(load_direction);
-  });
-  GEvent.addListener(dirn,"error", function() {
-    alert("Einer der eingegbenen Orte wurde nicht gefunden.Sie müssen einen gültigen Ort + Adresse oder Plz angeben");
-    console.log("Failed: "+dirn.getStatus().code);
-  });
-  GEvent.addListener(dirn,"load", function() {
-    console.log(dirn);
-    console.log("The Fahrt dauert is "+dirn.getDuration().seconds);
-    console.log("The distance is "+dirn.getDistance().meters);
-    var configJson = getConfigJson();
-    var lkw_matrix_json = getLKWMatrixJson();
+    directions_service.route(route_request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        console.log(response);
+        var route = response.routes[0].legs[0];
+        console.log(route.start_address);
+        $("#start").val(route.start_address);
+        $("#end").val(route.end_address);
+        console.log("Die Fahrt dauert "+route.duration.value);
+        console.log("Die Entfernung ist "+route.distance.value);
+        var configJson = getConfigJson();
+        var lkw_matrix_json = getLKWMatrixJson();
+        
+        var orig_distance = getOrigDistance(route);
+        $("#orig_distance").html(orig_distance);
+        console.log("----->orig_distance  "+orig_distance);
+        
+        var removal_time = getRemovalTime(configJson, route, rooms);
+        //$("#removal_time").html("Zeit des Umzugs (Fahrtzeit  (aufgerundet in Stunden ) + Be/Entladen (3 zimmer = 3 stunden * 2, größer = 4,5 *2  ))="+removal_time+" Stunden");
+        console.log("----->removal_time  "+removal_time);
+        
+        var lkw_rent_distance = getLKWRentDistance(lkw_matrix_json, orig_distance);
+        //$("#lkw_rent_distance").html("die LKW Miet Distanz ="+lkw_rent_distance+" km");
+        console.log("----->lkw_rent_distance  "+lkw_rent_distance);
     
-    var orig_distance = getOrigDistance(dirn);
-    $("#orig_distance").html("Die Distanz "+orig_distance+" km");
-    console.log("----->orig_distance  "+orig_distance);
-    
-    var removal_time = getRemovalTime(configJson, dirn, rooms);
-    $("#removal_time").html("Zeit des Umzugs (Fahrtzeit  (aufgerundet in Stunden ) + Be/Entladen (3 zimmer = 3 stunden * 2, größer = 4,5 *2  ))="+removal_time+" Stunden");
-    console.log("----->removal_time  "+removal_time);
-    
-    var lkw_rent_distance = getLKWRentDistance(lkw_matrix_json, orig_distance);
-    $("#lkw_rent_distance").html("die LKW Miet Distanz ="+lkw_rent_distance+" km");
-    console.log("----->lkw_rent_distance  "+lkw_rent_distance);
-    
-    var num_boxes = getNumBoxes(configJson);
-    $("#num_boxes").html("Anzahl der Kartons ( pro zimmer = 15 + pro mitglied = 4) ="+num_boxes);
-    console.log("----->num_boxes "+num_boxes);
-    
-    var price_boxes = parseFloat(getPriceBoxes(configJson, num_boxes));
-    $("#price_boxes").html("Preis der Kartons ( Preis pro Box = 1.50 € * anzahl der Boxen)  ="+price_boxes+"€");
-    console.log("----->price_boxes "+price_boxes);
-    
-    var lkw_size = getLKWSize(configJson, num_boxes);
-    $("#lkw_size").html("benötigte LKW Größe ( anzahl der boxen < 44 = 3,5T LKW ansonsten 7,5T LKW ) = "+lkw_size);
-    console.log("----->lkw_size "+lkw_size);
-    
-    var lkw_rent = parseInt(getLKWRent(lkw_matrix_json, lkw_rent_distance, lkw_size));
-    $("#lkw_rent").html("die LKW Miete (ermittelt aus LKW Matrix anhand der LKW Miet Distanz und der LKW Größe ) ="+lkw_rent+"€");
-    console.log("----->lkw_rent  "+lkw_rent);
-    
-    var fuel_price = parseFloat(getFuelPrice(configJson, orig_distance, lkw_size));
-    $("#fuel_price").html("der Dieselpreis für die Umzugsstrecke ( die Distanz in km * den verbrauch des lkws pro km  ) ="+fuel_price+"€");
-    console.log("----->fuel_price "+fuel_price);
-    
-    var stop_area_price_array = getStopAreaPrice(dirn.getRoute(0));
-    var stop_area_price_sum = (parseInt(stop_area_price_array[0]) + parseInt(stop_area_price_array[1]));
-    $("#stop_area_price").html("der gesamte Halteverbotspreis anhand einer Preistabelle für die 30 größten Städte Deutschlands ="+ stop_area_price_sum+"€");
-    console.log("----->start area price "+stop_area_price_array[0]+ " target area price "+stop_area_price_array[1]+" gesamt = "+ stop_area_price_sum);
-     
-    var catering_price = parseFloat(getCateringPrice(configJson, removal_time));
-    $("#catering_price").html("Verpflegungskosten (2.50 € pro mitglied und helfer * umzugszeit) ="+catering_price+"€");
-    console.log("----->catering_price "+catering_price);
-    
-    console.log("dirn routes= "+dirn.getNumRoutes());
-    
-    $("#sum").html("Gesamtpreis = "+(price_boxes+lkw_rent+fuel_price+stop_area_price_sum+catering_price)+"€");
-  }); 
-  $("#removal_form input:text").click( function () {
-    $(this).val("").unbind("click");
-  });
+        var num_boxes = getNumBoxes(configJson);
+        $("#num_boxes").html(num_boxes);
+        console.log("----->num_boxes "+num_boxes);
+        
+        var price_boxes = parseFloat(getPriceBoxes(configJson, num_boxes));
+        $("#price_boxes").html(price_boxes);
+        console.log("----->price_boxes "+price_boxes);
+        
+        var lkw_size = getLKWSize(configJson, num_boxes);
+        var lkw_size_out = (lkw_size == "t7" ? "7,5t" : "3,5t" );
+        
+        $("#lkw_size").html(lkw_size_out);
+        console.log("----->lkw_size "+lkw_size_out);
+        
+        var lkw_rent = parseInt(getLKWRent(lkw_matrix_json, lkw_rent_distance, lkw_size));
+        $("#lkw_rent").html(lkw_rent);
+        console.log("----->lkw_rent  "+lkw_rent);
+
+        var fuel_price = parseFloat(getFuelPrice(configJson, orig_distance, lkw_size));
+        $("#fuel_price").html(fuel_price);
+        var mileage = (lkw_size == "t7" ? "15l" : "10l");
+        $("#mileage").html(mileage);
+        console.log("----->fuel_price "+fuel_price);
+        
+        var stop_area_price_array = getStopAreaPrice(route);
+        var stop_area_price_sum = (parseInt(stop_area_price_array[0]) + parseInt(stop_area_price_array[1]));
+        $("#stop_area_price").html(stop_area_price_sum);
+        console.log("----->start area price "+stop_area_price_array[0]+ " end area price "+stop_area_price_array[1]+" gesamt = "+ stop_area_price_sum);
+        
+        var catering_price = parseFloat(getCateringPrice(configJson, removal_time));
+        $("#catering_price").html(catering_price);
+        console.log("----->catering_price "+catering_price);
+        
+        var sum = price_boxes+lkw_rent+fuel_price+stop_area_price_sum+catering_price;
+        $("#sum .own_removal_val span").html(sum);
+        $("#header_sum span i").html(sum);
+        
+        
+      } else {
+        alert("Einer der eingegbenen Orte wurde nicht gefunden.Sie müssen einen gültigen Ort + Adresse oder Plz angeben");
+      }
+    });
+  };
 });
 getConfigJson = function() {
   var temp = null;
@@ -107,20 +141,23 @@ getLKWMatrixJson = function() {
   });
   return (temp);
 }
-getOrigDistance = function(dirn){
+getOrigDistance = function(route){
   var temp = null;
-  temp = dirn.getDistance().meters;
+  temp = route.distance.value;
   temp = parseInt(temp/1000);
   return(temp);
 }
-getRemovalTime = function(configJson, dirn, rooms){
+getRemovalTime = function(configJson, route, rooms){
   var STATIC_drive_rest_time_in_sec = parseInt(configJson.drive_rest_time_in_sec);
-  var STATIC_shipping_time_for_max_3_rooms_in_sec = parseInt(configJson.shipping_time_for_max_3_rooms_in_sec);//start and target 
-  var STATIC_shipping_time_for_more_3_rooms_in_sec = parseInt(configJson.shipping_time_for_more_3_rooms_in_sec);//start and target
+  var STATIC_shipping_time_for_max_3_rooms_in_sec = parseInt(configJson.shipping_time_for_max_3_rooms_in_sec);//start and end 
+  var STATIC_shipping_time_for_more_3_rooms_in_sec = parseInt(configJson.shipping_time_for_more_3_rooms_in_sec);//start and end
   var STATIC_room_limit = parseInt(configJson.room_limit);
   var temp = null;
-  temp = dirn.getDuration().seconds;
-  temp =  Math.round(((temp+STATIC_drive_rest_time_in_sec+( rooms > STATIC_room_limit ? STATIC_shipping_time_for_more_3_rooms_in_sec : STATIC_shipping_time_for_max_3_rooms_in_sec))/60)/60);
+  temp = route.duration.value;
+  console.log(temp);
+  temp =  ((temp+STATIC_drive_rest_time_in_sec+( rooms > STATIC_room_limit ? STATIC_shipping_time_for_more_3_rooms_in_sec : STATIC_shipping_time_for_max_3_rooms_in_sec))/60)/60;
+  temp = Math.round(temp);
+  console.log(temp);console.log("in Stunden");
   return(temp);
 }
 getLKWRentDistance = function (lkw_matrix_json, orig_distance) {
@@ -166,18 +203,17 @@ getFuelPrice = function(configJson, orig_distance, lkw_size) {
   var average_fuel = parseInt(orig_distance*(lkw_size == "t3" ? STATIC_average_fuel_consum_1_km_t3 : STATIC_average_fuel_consum_1_km_t7 ));
   return (average_fuel*STATIC_fuel_price_liter);
 }
-getStopAreaPrice = function(g_route) {
-  console.log(g_route.getSummaryHtml());
-   console.log("------->"+g_route.getStartGeocode().address );
-   console.log("------->"+g_route.getEndGeocode().address );
+getStopAreaPrice = function(route) {
+   console.log("------->"+route.start_address);
+   console.log("------->"+route.end_address);
   var temp  =  new Array();
   var startLoc;
-  var targetLoc;
-  startLoc = g_route.getStartGeocode().address.split(",");
+  var endLoc;
+  startLoc = route.start_address.split(",");
   startLoc = startLoc[startLoc.length -2].replace(/\d/g,"").trim();
-  targetLoc = g_route.getEndGeocode().address.split(",");
-  targetLoc = targetLoc[targetLoc.length -2].replace(/\d/g,"").trim();
-  console.log("von "+startLoc +" nach "+ targetLoc );
+  endLoc = route.end_address.split(",");
+  endLoc = endLoc[endLoc.length -2].replace(/\d/g,"").trim();
+  console.log("von "+startLoc +" nach "+ endLoc );
   $.ajax({
       type: "GET",
       url: "stop_area_prices.json",
@@ -186,10 +222,10 @@ getStopAreaPrice = function(g_route) {
       success: function(data){
         var start_price = (data[startLoc] != undefined ? data[startLoc] : 0 );
         console.log("start_price="+start_price);
-        var target_price = (data[targetLoc]!= undefined ? data[targetLoc] : 0 );
-        console.log("target_price="+target_price);
+        var end_price = (data[endLoc]!= undefined ? data[endLoc] : 0 );
+        console.log("end_price="+end_price);
         temp.push(start_price);
-        temp.push(target_price);
+        temp.push(end_price);
         console.log(temp);
       }
   });
